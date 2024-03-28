67942a80eacf3801907a7eacab6d852866a6eb0b
Yixun Xu <yixunx@palantir.com>
2024-03-28 11:30:10 -0400

    tweak

--- lucene/queries/src/test/org/apache/lucene/queries/spans/TestSpanNearQueryClauseLimit.java
+++ lucene/queries/src/test/org/apache/lucene/queries/spans/TestSpanNearQueryClauseLimit.java
@@ -15,17 +15,17 @@ import java.util.UUID;
 public class TestSpanNearQueryClauseLimit extends LuceneTestCase {
 
     private static final String FIELD_NAME = "field";
-    private static final int NUM_DOCUMENTS = 5000;
+    private static final int NUM_DOCUMENTS = 1025;
 
     /**
-     * Creates an index with NUM_DOCUMENTS documents. Each document has a text field in the form of "abc test foo_".
+     * Creates an index with NUM_DOCUMENTS documents. Each document has a text field in the form of "abc foo bar_[UUID]".
      */
     private Directory createIndex() throws Exception {
         Directory dir = newDirectory();
         try (IndexWriter writer = new IndexWriter(dir, new IndexWriterConfig())) {
             for (int i = 0; i < NUM_DOCUMENTS; i++) {
                 Document doc = new Document();
-                doc.add(new TextField("field", "abc test foo_" + UUID.randomUUID(), Field.Store.YES));
+                doc.add(new TextField("field", "abc foo bar_" + UUID.randomUUID(), Field.Store.YES));
                 writer.addDocument(doc);
             }
             writer.commit();
@@ -33,15 +33,16 @@ public class TestSpanNearQueryClauseLimit extends LuceneTestCase {
         return dir;
     }
 
+    // SpanNearQuery hits limit of 1024 clauses
     public void testSpanNearQueryClauseLimit() throws Exception {
         Directory dir = createIndex();
 
-        // Find documents that match "test foo.*", which should match all documents.
+        // Find documents that match "abc <some term> bar.*", which should match all documents.
         try (IndexReader reader = DirectoryReader.open(dir)) {
             Query query = new SpanNearQuery.Builder(FIELD_NAME, true)
-                    .setSlop(0)
-                    .addClause(new SpanTermQuery(new Term(FIELD_NAME, "test")))
-                    .addClause(new SpanMultiTermQueryWrapper<>(new PrefixQuery(new Term(FIELD_NAME, "foo"))))
+                    .setSlop(1)
+                    .addClause(new SpanTermQuery(new Term(FIELD_NAME, "abc")))
+                    .addClause(new SpanMultiTermQueryWrapper<>(new PrefixQuery(new Term(FIELD_NAME, "bar"))))
                     .build();
 
             // This throws exception if NUM_DOCUMENTS is > 1024.
@@ -56,21 +57,23 @@ public class TestSpanNearQueryClauseLimit extends LuceneTestCase {
         dir.close();
     }
 
+    // It seems that if slop = 0 (e.g. searching for "foo bar.*") then MultiPhraseQuery can be used instead, and this
+    // doesn't hit the clause limit because MultiPhraseQuery doesn't construct one sub-query for each term.
     public void testMultiPhraseQuery() throws Exception {
         Directory dir = createIndex();
 
-        // Find documents that match "test foo.*", which should match all documents.
+        // Find documents that match "foo bar.*", which should match all documents.
         try (IndexReader reader = DirectoryReader.open(dir)) {
             IndexSearcher searcher = newSearcher(reader);
             MultiPhraseQuery.Builder querybuilder = new MultiPhraseQuery.Builder();
 
-            // Add the first term "test"
-            querybuilder.add(new Term(FIELD_NAME, "test"));
+            // Add the first term "foo"
+            querybuilder.add(new Term(FIELD_NAME, "foo"));
 
             LinkedList<Term> termsWithPrefix = new LinkedList<>();
 
-            // Find all terms that start with the second term "foo"
-            String prefix = "foo";
+            // Find all terms that start with the second term "bar"
+            String prefix = "bar";
             TermsEnum termsEnum = MultiTerms.getTerms(reader, FIELD_NAME).iterator();
             termsEnum.seekCeil(new BytesRef(prefix));
             do {
